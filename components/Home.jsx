64557aad01242197073d0d3801afc3938c1ea0c4
Piyush Arora <74443858+Piyusharora2003@users.noreply.github.com>
2024-06-15 23:44:45 +0530

    UI fixes

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,47 +1,18 @@
 "use client";
 import React, {useState} from 'react'
 import { UserDataBlock } from './Blocks/UserData.jsx';
-import { MdEmojiPeople } from "react-icons/md";
-import { ShepherdJourneyProvider, useShepherd } from "react-shepherd";
-import { Steps } from '@/app/steps.js';
 import  ImageGallery  from './Blocks/ImageGallery.jsx';
 // Home.jsx
 
 import { BMI, BMR, IBW, RecomendedExercise, BFP } from './Blocks';
 
 function Home({userData}) {
-    const shepherd = useShepherd();
-    const [tourState, setTourState] = useState(false);
-    const tour = new shepherd.Tour({
-        useModalOverlay: true,
-        defaultStepOptions: {
-          cancelIcon: true,
-          scrollTo: false,
-          classes:
-            "bg-base-100 shadow-xl p-5 w-96 rounded-lg border-2 border-indigo-500 mt-3",
-        },
-        steps: Steps,
-      });
-      
-      function startTour(){
-        if(tourState) {
-            alert('Tour already started');
-            return;
-        }
-        tour.start();
-        setTourState(true);
-      }
 
   return (
-    <ShepherdJourneyProvider>
-    <div className='flex'>
-        <button 
-            onClick={startTour}
-            className='absolute bottom-6 right-6 text-5xl bg-yellow-600 text-black hover:bg-sky-700 rounded-full p-2'
-        >
-            <MdEmojiPeople />
-        </button>
-        <div className="float">
+    <>
+    <div className='flex flex-wrap md:flex-nowrap mx-auto'>
+      
+        <div className="float mx-auto">
             <UserDataBlock userData= {userData} />
             <div id='analyticsBlocks'>
                 <BMI userData = {userData} />
@@ -49,16 +20,19 @@ function Home({userData}) {
                 <IBW userData = {userData} />
             </div>
         </div>
-        <div className='float-left'>
+        <div className='float-left mx-auto'>
             <BFP userData = {userData} />
             <RecomendedExercise userData = {userData} />
         </div>
-        <div className='float-left'>
-            <ImageGallery />
-        </div>
+        {
+            window.screen.width > 768 &&
+            <div className='float-left md:flex'>
+                <ImageGallery />
+            </div>
+        }
     </div>
-    </ShepherdJourneyProvider>
-  )
+    </>
+)
 }
 
 export default Home;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,35 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { GrPowerReset } from "react-icons/gr";
-
+import { MdEmojiPeople } from "react-icons/md";
+import {  useShepherd } from "react-shepherd";
+import { Steps } from '@/app/steps.js';
 
 function Navbar({showResetBtn}) {
+
+  const shepherd = useShepherd();
+  const [tourState, setTourState] = useState(false);
+  const tour = new shepherd.Tour({
+      useModalOverlay: true,
+      defaultStepOptions: {
+        cancelIcon: true,
+        scrollTo: false,
+        classes:
+          "bg-base-100 shadow-xl p-5 w-96 rounded-lg border-2 border-indigo-500 mt-3",
+      },
+      steps: Steps,
+    });
+    
+    function startTour(){
+      if(tourState) {
+          alert('Tour already started');
+          return;
+      }
+      tour.start();
+      setTourState(true);
+    }
   return (
-        <div className="flex-1 justify-between px-2">
+        <div className="flex-1 w-full justify-between px-2">
             <Link
               href="#"
               className="btn btn-ghost text-xl text-accent"
@@ -14,17 +38,24 @@ function Navbar({showResetBtn}) {
               Health Tracker
             </Link>
             {
-              showResetBtn &&
-            <div 
-            className='float-right	text-white font-extrabold	text-xl cursor-pointer ' 
-            id='Reset'
-            onClick={() => {
-              localStorage.removeItem("UserData_Health_Tracker");
-              localStorage.removeItem("locShared");
-              window.location.reload();
-            }}  
-            >   
-              <GrPowerReset/>
+              showResetBtn && <div className='flex gap-4'>
+              <div 
+              className='float-right p-auto m-auto text-white font-extrabold	text-xl cursor-pointer ' 
+              id='Reset'
+              onClick={() => {
+                localStorage.removeItem("UserData_Health_Tracker");
+                localStorage.removeItem("locShared");
+                window.location.reload();
+              }}  
+              >   
+                <GrPowerReset/>
+              </div>
+              <button 
+                onClick={startTour}
+                className=' bottom-6 right-6 text-xl bg-yellow-600 text-black hover:bg-sky-700 rounded-full p-2'
+              >
+                <MdEmojiPeople />
+              </button>
             </div>
           }
         </div>
